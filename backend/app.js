a243734249171f61b3d2723446ba7e454cdae51a
profsamuelrodrigues <55418418+profsamuelrodrigues@users.noreply.github.com>
2024-10-20 19:20:40 -0300

    Estrutura Inicial do backend

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+require("dotenv").config()
+
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+
+
+const port = process.env.PORT
+
+const app = express()
+
+//resolver prblemas de cors
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }))
+
+//Configurar resposta em json e form data
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//pasta de upload de imagens
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+
+//rotas
+app.get('/', (req, res) => {
+    res.send('Bem vindo')
+})
+//const router = require("./routes/Router.js")
+//app.use(router)
+
+//Conexão com o banco de dados
+require("./config/db.js")
+
+
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const dbUser = process.env.DB_USER
+const dbPassord = process.env.DB_PASSWORD
+
+//conexão com o banco de dados
+const conn = async () => {
+    try {
+        //const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassord}@cluster0.wmdy9lc.mongodb.net/dbAlunos`)
+        'mongodb://localhost:27017/reactgram'
+        console.log("Conectado ao banco de dados")
+        return conn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn
